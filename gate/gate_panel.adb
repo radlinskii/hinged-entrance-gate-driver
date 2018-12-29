37b865fab8cde82949b30e5a40865fc9127a08cd
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2018-12-29 19:38:39 +0100

    create gate panel

--- gate/gate_panel.adb
+++ gate/gate_panel.adb
@@ -1,13 +1,84 @@
 with System;
 with Gate_Pack; pragma Unreferenced(Gate_Pack);
+use Gate_Pack;
 with Ada.Text_IO;
 use  Ada.Text_IO;
 
+with Ada.Strings;
+use Ada.Strings;
+with Ada.Strings.Fixed;
+use Ada.Strings.Fixed;
+
+with Ada.Calendar;
+use Ada.Calendar;
+
+with Ada.Text_IO, Ada.Integer_Text_IO;
+
 procedure Gate_Panel is
+
+  type Atrybuty is (Czysty, Jasny, Podkreslony, Negatyw, Migajacy, Szary);
+
+  protected Screen  is
+    procedure Print_XY(X,Y: Positive; S: String; Atryb : Atrybuty := Czysty);
+    procedure Clear;
+    procedure Background;
+    procedure Configuration;
+  end Screen;
+  
+  protected body Screen is
+    -- implementacja dla Linuxa i macOSX
+    function Atryb_Fun(Atryb : Atrybuty) return String is 
+      (case Atryb is 
+       when Jasny => "1m", when Podkreslony => "4m", when Negatyw => "7m",
+       when Migajacy => "5m", when Szary => "2m", when Czysty => "0m"); 
+       
+    function Esc_XY(X,Y : Positive) return String is 
+      ( (ASCII.ESC & "[" & Trim(Y'Img,Both) & ";" & Trim(X'Img,Both) & "H") );   
+       
+    procedure Print_XY(X,Y: Positive; S: String; Atryb : Atrybuty := Czysty) is
+      Before : String := ASCII.ESC & "[" & Atryb_Fun(Atryb);              
+    begin
+      Put( Before);
+      Put( Esc_XY(X,Y) & S);
+      Put( ASCII.ESC & "[0m");
+    end Print_XY;
+    
+    procedure Clear is
+    begin
+      Put(ASCII.ESC & "[2J");
+    end Clear;   
+    
+    procedure Background is
+    begin
+      Screen.Clear;
+      Screen.Print_XY(1,1,"+=========== The Gate ===========+");
+      Screen.Print_XY(10, 3, "State: ");
+      Screen.Print_XY(5, 5, "Axis: ");
+    end Background; 
+
+    procedure Configuration is
+    begin
+      Screen.Clear;
+      Screen.Print_XY(1,1,"+=========== Configure your gate ===========+");
+      Screen.Print_XY(3, 3, "How much time for pause (seconds): ");
+    end Configuration; 
+        
+  end Screen;
+
   pragma Priority (System.Priority'First);
+  S : State;
+  Next : Ada.Calendar.Time;
+  Shift : constant Duration := 0.5;
+  Pause_Time : Integer;
+  Axis : Natural;
 begin
-  Put_Line("Brama: początek");
-  loop
-    null;
-  end loop;
+  Screen.Configuration;
+  Ada.Integer_Text_IO.Get(Pause_Time);
+  loop 
+    Screen.Background; 
+    Gate.Get_State(S);
+    Screen.Print_XY(17, 3, S'Img, Atryb=>Negatyw);
+    Next := Clock + Shift;
+    delay until Next;
+  end loop; 
 end Gate_Panel;
